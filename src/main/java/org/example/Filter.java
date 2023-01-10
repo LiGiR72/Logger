fb92da4e6bb77986d19c7e2006f5df890e8d30b9
LiGiR72 <iliya.alabyew@gmail.com>
2023-01-10 05:06:56 +0300

    Formating

--- src/main/java/org/example/Filter.java
+++ src/main/java/org/example/Filter.java
@@ -10,20 +10,20 @@ public class Filter {
         this.threshold = threshold;
     }
 
-    public List<Integer> filterOut(List<Integer> source){
+    public List<Integer> filterOut(List<Integer> source) {
         Logger logger = Logger.getInstance();
         List<Integer> result = new ArrayList<>();
         int i = 0;
         logger.log("Запускаем фильтрацию");
-        for (Integer s:source) {
-            if(s <= threshold ){
+        for (Integer s : source) {
+            if (s <= threshold) {
                 result.add(s);
                 i++;
             }
-            logger.log("Элемент " + s + ((s <= threshold )?"":" не") + " проходит");
+            logger.log("Элемент " + s + ((s <= threshold) ? "" : " не") + " проходит");
         }
         logger.log("Прошло фильтр " + i + " элемнтов из " + source.size());
-        return  result;
+        return result;
     }
 
 }
